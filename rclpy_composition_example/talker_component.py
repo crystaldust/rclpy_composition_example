9cd0dcc6918fbd0c137d19dc65a7ebf9ebad72d7
Zhen Ju <lance@arch-pc.localdomain>
2020-06-29 09:11:54 +0800

    Remove the *args parameter for talker component's constructor.

--- rclpy_composition_example/talker_component.py
+++ rclpy_composition_example/talker_component.py
@@ -3,9 +3,9 @@ from rclpy.node import Node
 
 
 class Talker(Node):
-    def __init__(self, node_name, *kargs, topic_name="py_composition_topic", timer_period=1, **kwargs):
-        super().__init__(node_name, *kargs, **kwargs)
-        self.publisher_ = self.create_publisher(String, topic_name, 10)
+    def __init__(self, node_name, topic_name="py_composition_topic", timer_period=1, **kwargs):
+        super().__init__(node_name, **kwargs)
+        self.publisher_ = self.create_publisher(String, topic_name, timer_period)
         self.timer_ = self.create_timer(timer_period, self.on_timer)
         self.i = 0
 
